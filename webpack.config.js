56a2e8c3ffc680c4e0a5933c109bd68578b6c144
Ranayke Boni Ferreira <ranayke2009@gmail.com>
2023-01-25 21:56:44 -0300

    First configurations and connected API

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path')
+
+module.exports = {
+    entry: [path.resolve(__dirname, './src/main.js')],
+    output: {
+        path: path.resolve(__dirname, './public'),
+        filename: 'bundle.js',
+        sourceMapFilename: 'bundle.js.map'
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, './public'),
+        },
+        port: 3000
+    },
+    devtool: 'source-map'
+}
